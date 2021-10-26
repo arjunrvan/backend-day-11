f394f57affe3ada20d70f449adb645c4ad8da63f
arjunrvan <arjunrvan@gmail.com>
2021-10-26 09:30:14 +0800

    updated

--- controller/tacCont.php
+++ controller/tacCont.php
@@ -3,7 +3,7 @@
     function insertTac () {
         include "model/customersModel.php";
         include "model/tacModel.php";
-        $tac = random_int(100000, 999999);
+        // $tac = random_int(100000, 999999);
         $cID = selectRecord('id');
         // Insert into tac table
         // insertRecordTac('tac','customer_id',$tac,$cID);
